eea2d9813f72f79241ca10c4df125b926984c5fa
yang <is_yang@163.com>
2016-09-17 02:13:52 +0800

    第19章完

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learning_logs',
+    'users',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +103,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 我的设置
+LOGIN_URL = '/users/login/'
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('learning_logs.urls', namespace='learning_logs')),
+    url(r'users/', include('users.urls', namespace='users')),
 ]
